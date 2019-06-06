99b1cef7ed5dc1f14791a4b56cc8d034967d6eb5
Ben Bozzay <ben@fullstackdigital.com>
2019-06-06 15:41:47 -0400

    refactoring work grid animation and image lazyload animation dependencies

---
+++ themes/fullstackdigital/assets/js/util.js
@@ -0,0 +1,84 @@
+if (!Util) {
+
+function Util () {};
+
+Array.prototype.diff = function(arr2) {
+  var ret = [];
+  this.sort();
+  arr2.sort();
+  for(var i = 0; i < this.length; i += 1) {
+      if(arr2.indexOf(this[i]) === -1){
+          ret.push(this[i]);
+      }
+  }
+  return ret;
+};
+
+Util.imagesLoaded = () => {
+  let lazyImages = Array.from(document.querySelectorAll("img[data-src]"));
+  let ignoreClasslist = Array.from(document.querySelectorAll(".gridgrow-image"));
+  
+  let images = lazyImages.diff(ignoreClasslist)
+  console.log("image length", images)
+  if (lazyImages.length > 0) {
+    
+    images.forEach(function (image, imageLoaded = 0) {
+      
+      image.classList.add("lazyload", "lazypreload");
+      image.addEventListener("load", () => {
+        imageLoaded++;
+        //console.log("loaded...", image)
+        if (images.length === imageLoaded) {
+          //console.log("all images loaded")
+          if (ignoreClasslist.length > 0) {
+            // For now, if there is an ignoreclasslist, another function will probably stop the animation. so don't stop it here.
+          } else {
+            Util.loadingAnimation(false);
+          }
+          
+        } 
+      })
+    });
+  } else {
+    Util.loadingAnimation(false);
+  }
+  // check if an element exists in array using a comparer function
+  // comparer : function(currentElement)
+  //console.log(images)
+
+    // Add lazyload class to images that are added via content editor, since it's difficult to modify that markup
+    
+}
+Util.loadingAnimation = (direction = true) => {
+  //console.log("loading..direction:", direction)
+  if (direction === false) {
+    setTimeout(function(){
+      document.querySelector("html").classList.remove("pageTransition", "loading");
+      //document.body.classList.remove("loading");
+      AOS.init({
+        offset: 0,
+        once: true,
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+      });
+    }, 1000)
+    
+  } if (direction === true) {
+    document.querySelector("html").classList.add("loading");
+    Util.imagesLoaded();
+    //document.body.classList.add("loading");
+  }
+}
+
+(() => {
+  Util.loadingAnimation();
+  
+  //add simple support for background images:
+  document.addEventListener('lazybeforeunveil', function(e){
+      var bg = e.target.getAttribute('data-bg');
+      if(bg){
+          e.target.style.backgroundImage = 'url(' + bg + ')';
+      }
+  });
+})();
+}
