5af4f579938d7152f352ba9cda1bb2f5c91d202c
bbozzay <bbozzay@gmail.com>
2019-10-02 09:59:04 -0400

    on work grid scroll, the reversebtn changes color based on the overlapping section skin

--- themes/fullstackdigital/assets/js/app.js
+++ themes/fullstackdigital/assets/js/app.js
@@ -12,8 +12,63 @@ document.addEventListener("DOMContentLoaded", function () {
         bodyScrollLock.enableBodyScroll(document.body);
     }
     AOS.init();
+    setSectionSkins();
 }, false)
 
+function setSectionSkins () {
+    let sections = document.querySelectorAll("section");
+
+    sections.forEach(section => {
+        let bgColor = getComputedStyle(section).backgroundColor;
+        let skin = lightOrDark(bgColor);
+        console.log(section, bgColor, skin)
+
+        section.setAttribute("skin", skin)
+    });
+}
+function lightOrDark(color) {
+
+    // Variables for red, green, blue values
+    var r, g, b, hsp;
+    
+    // Check the format of the color, HEX or RGB?
+    if (color.match(/^rgb/)) {
+
+        // If HEX --> store the red, green, blue values in separate variables
+        color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+        
+        r = color[1];
+        g = color[2];
+        b = color[3];
+    } 
+    else {
+        
+        // If RGB --> Convert it to HEX: http://gist.github.com/983661
+        color = +("0x" + color.slice(1).replace( 
+        color.length < 5 && /./g, '$&$&'));
+
+        r = color >> 16;
+        g = color >> 8 & 255;
+        b = color & 255;
+    }
+    
+    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(
+    0.299 * (r * r) +
+    0.587 * (g * g) +
+    0.114 * (b * b)
+    );
+
+    // Using the HSP value, determine whether the color is light or dark
+    if (hsp>127.5) {
+
+        return 'light';
+    } 
+    else {
+
+        return 'dark';
+    }
+}
 
 // Object associated with a history entry
 // Add default values for the first page load
@@ -29,6 +84,7 @@ let changeWindowContent = () => {
     AjaxScriptLoader().isLoading(false);
     window.scrollTo (0,0);
     toggleActiveState();
+    setSectionSkins();
 }
 
 // Call this function
